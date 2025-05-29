dc31ab9d6be1d4e06c47a7e9aa587a3ff1b58f67
fiftd <dlarlgus20@gmail.com>
2025-05-29 15:31:26 +0900

    Revert "new enchant_rate : rate table of enchant" This reverts commit c967f41b75fbf598d13c0e35faa6c2f576f471e0.

--- enchant.py
+++ enchant.py
@@ -1,7 +1,8 @@
-import rate
+import enchant_rate
 
 def enchant(equipment, enchant_level, mini_game_result=0):
-    result = rate.enchant_rate(enchant_level, mini_game_result)
+    print("mini_game_result in enchant:", mini_game_result)
+    result = enchant_rate.enchant_rate(enchant_level, mini_game_result)
     if result == 0:
         enchant_level = 0
         equipment["image1"] = equipment["image2"]
